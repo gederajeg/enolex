5044072b0207e98fd8839e78002be7c95662f11b
Enggano <engganolang@gmail.com>
2024-03-11 14:05:09 +0800

    Update profiling code for van De Straten, Walland, and Francis.

--- codes/r-code_04-1-orthography-profiling.R
+++ codes/r-code_04-1-orthography-profiling.R
@@ -102,3 +102,97 @@ vrosen_str <- vrosen$strings |>
 ### combine with the main data ====
 vrosenberg <- vrosenberg |> 
   left_join(vrosen_str, by = join_by(ortho_id))
+
+
+
+# Van de Straten & Severijn 1855 ====
+vdstraten <- eno_etym_long_mini8 |> 
+  filter(EngganoSource == "vd Straten & S. 1855") |> 
+  mutate(ortho_id = row_number())
+## create a skeleton profile for Van de Straten & Severijn 1855 ====
+# qlcData::write.profile(vdstraten$words, normalize = "NFC", editing = TRUE, info = FALSE,
+#                        file.out = "ortho/_04-vdStraten1855_profile-skeleton.tsv")
+### segmentise and transliterate after editing the skeleton profile ====
+vds <- qlcData::tokenize(vdstraten$words, 
+                            profile = "ortho/_04-vdStraten1855_profile-skeleton.tsv", 
+                            file.out = "ortho/_04-vdStraten1855",
+                            method = "global", 
+                            transliterate = "Replacement", 
+                            ordering = NULL, # cf. Moran & Cysouw (2018: 112-114)
+                            normalize = "NFC", 
+                            sep.replace = "#")
+
+### tidying up the segmentised and transliterated table ====
+vds_str <- vds$strings |> 
+  as_tibble() |> 
+  mutate(ortho_id = row_number()) |> 
+  mutate(commons = str_replace_all(transliterated, "\\s{1}", ""),
+         commons = str_replace_all(commons, "\\#", " ")) |> 
+  select(ortho_id, everything())
+
+### combine with the main data ====
+vdstraten <- vdstraten |> 
+  left_join(vds_str, by = join_by(ortho_id))
+
+
+
+# Walland 1864 ====
+walland <- eno_etym_long_mini8 |> 
+  filter(EngganoSource == "Walland 1864") |> 
+  mutate(ortho_id = row_number())
+## create a skeleton profile for Walland 1864 ====
+# qlcData::write.profile(walland$words, normalize = "NFC", editing = TRUE, info = FALSE,
+#                        file.out = "ortho/_05-walland1864_profile-skeleton.tsv")
+### segmentise and transliterate after editing the skeleton profile ====
+wlnd <- qlcData::tokenize(walland$words, 
+                         profile = "ortho/_05-walland1864_profile-skeleton.tsv", 
+                         file.out = "ortho/_05-walland1864",
+                         method = "global", 
+                         transliterate = "Replacement", 
+                         ordering = NULL, # cf. Moran & Cysouw (2018: 112-114)
+                         normalize = "NFC", 
+                         sep.replace = "#")
+
+### tidying up the segmentised and transliterated table ====
+wlnd_str <- wlnd$strings |> 
+  as_tibble() |> 
+  mutate(ortho_id = row_number()) |> 
+  mutate(commons = str_replace_all(transliterated, "\\s{1}", ""),
+         commons = str_replace_all(commons, "\\#", " ")) |> 
+  select(ortho_id, everything())
+
+### combine with the main data ====
+walland <- walland |> 
+  left_join(wlnd_str, by = join_by(ortho_id))
+
+
+
+# Francis 1870 ====
+francis <- eno_etym_long_mini8 |> 
+  filter(EngganoSource == "Francis 1870") |> 
+  mutate(ortho_id = row_number())
+## create a skeleton profile for Francis 1870 ====
+# qlcData::write.profile(francis$words, normalize = "NFC", editing = TRUE, info = FALSE,
+#                        file.out = "ortho/_06-francis1870_profile-skeleton.tsv")
+### segmentise and transliterate after editing the skeleton profile ====
+frc <- qlcData::tokenize(francis$words, 
+                         profile = "ortho/_06-francis1870_profile-skeleton.tsv", 
+                         file.out = "ortho/_06-francis1870",
+                         method = "global", 
+                         transliterate = "Replacement", 
+                         ordering = NULL, # cf. Moran & Cysouw (2018: 112-114)
+                         normalize = "NFC", 
+                         sep.replace = "#",
+                         regex = FALSE)
+
+### tidying up the segmentised and transliterated table ====
+frc_str <- frc$strings |> 
+  as_tibble() |> 
+  mutate(ortho_id = row_number()) |> 
+  mutate(commons = str_replace_all(transliterated, "\\s{1}", ""),
+         commons = str_replace_all(commons, "\\#", " ")) |> 
+  select(ortho_id, everything())
+
+### combine with the main data ====
+francis <- francis |> 
+  left_join(frc_str, by = join_by(ortho_id))
