846cbcd3f0d8496097ec105012cad828c355efbb
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2024-06-05 07:01:30 +0100

    Add the BASH code for running pyconcepticon for EnoLEX gloss.

---
+++ codes/sh-code_01-pyconcepticon.sh
@@ -0,0 +1,21 @@
+# running pyconcepticon on terminal (on Git Bash Windows)
+## assuming I created a new virtual environment ".venv" in `cldf` directory using the following commands
+cd "C:\Users\GRajeg\OneDrive - Nexus365\Documents\cldf"
+python -m venv .venv
+
+## then already installed the relevant Python packages
+pip install pyconcepticon
+
+## and already downloaded the Concepticon database in the `concepticon-data/concepticondata` directory
+
+# go the the relevant directory
+cd "C:\Users\GRajeg\OneDrive - Nexus365\Documents\cldf"
+
+# activate the virtual environment
+source .venv/Scripts/activate
+
+# go the concepticon directory
+cd concepticon-data
+
+# run the concept mapping
+concepticon map_concepts C:/Users/GRajeg/OneDrive\ -\ Nexus365/Documents/Research/enolex-2023-05-23/data/enolex-gloss-to-map_2024-1810.tsv --output C:/Users/GRajeg/OneDrive\ -\ Nexus365/Documents/Research/enolex-2023-05-23/data/enolex-gloss-mapped-to-edit_2024-1810.tsv
