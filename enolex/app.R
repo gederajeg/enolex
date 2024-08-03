3570d48fa802c358ed49588f791450d29c0b7718
Enggano <engganolang@gmail.com>
2024-08-03 09:54:54 +0800

    Update the EnoLEX app source code.

--- enolex/app.R
+++ enolex/app.R
@@ -33,38 +33,45 @@ bibs1 <- select(bibs,
   select(YEAR, CITATION)
 
 english_gloss <- selectizeInput(inputId = "English_Gloss", 
+                                options = list(dropdownParent = "body"),
                                 label = "Concepts",
                                 choices = NULL, 
                                 selected = NULL
 )
+
+link_enolex_github <- tags$a(shiny::icon("github"), "GitHub", href="https://github.com/engganolang/enolex", target="_blank")
+link_enggano_web <- tags$a(shiny::icon("globe", lib = "glyphicon"), "Enggano webpage", href="https://enggano.ling-phil.ox.ac.uk/", target="_blank")
 bibs <- selectizeInput(inputId = "References",
                        label = "Sources",
                        choices = NULL, 
                        selected = NULL)
 
 cards <- list(
-  background_image = card(
-    full_screen = T,
-    card_image(file = "estuary.JPG",
-               fill = TRUE,
-               class = "border-0"),
-    card_footer("The estuary towards the Indian Ocean from the Bak Blau lake, Enggano Island",
-                class = "fs-6; fst-italic")
-  ),
-  citation = card(
-    full_screen = T,
-    card_body(card_title(div(h4("How to cite EnoLEX"), style="font-family:georgia")),
-              div(p(class = "text-muted", "Krauße, Daniel, Gede Primahadi Wijaya Rajeg, Cokorda Pramartha, Erik Zoebel, Charlotte Hemmings, I Wayan Arka, Mary Dalrymple (2024).", em("EnoLEX: A Diachronic Lexical Database for the Enggano language.")), style="font-family:georgia"),
-              div(h4(class = "text-muted", "Please also cite the following paper:"), style="font-family:georgia"),
-              div(p(class = "text-muted", "Rajeg, Gede Primahadi Wijaya, Daniel Krauße, and Cokorda Rai Adi Pramartha (2024).", a("EnoLEX: A Diachronic Lexical Database for the Enggano language", href='https://enggano.ling-phil.ox.ac.uk/static/papers/EnoLEX%20-%20A%20Diachronic%20Lexical%20Database%20for%20the%20Enggano%20language%20[Preprint].pdf', target="_blank"), ". In", em("Proceedings of AsiaLex 2024 (The Asian Association for Lexicography 2024 Hybrid Conference)."), "Toyo University, Tokyo: Japan."), style="font-family:georgia")
-    )),
-  enolex_description = card(
-    full_screen = T,
-    card_body(card_title(h3("Overview")),
-              p("EnoLEX collates lexical data from", actionLink("SourcesTabLink", "legacy materials and contemporary fieldwork data"), "about the Enggano language, ranging from simple/short and extensive word lists, anthropological and ethnographic writings, a dictionary, thesis, and contemporary Enggano data. The materials span over 150 years from the middle of the 19th century up to the present. With expert cognate-judgement, EnoLEX offers historical development of word forms expressing a certain concept/meaning."),
-              h4("How to get started"),
-              p("Users can go to the", actionLink("CognatesTabLink", "Cognates"), "tab and then, from the left-hand side sidebar, select the concept to filter forms expressing that concept and how they develop across periods.")),
-    textOutput("overview")
+  background_image = 
+    
+    card(card_image("estuary.JPG", border_radius = "none"),
+         card_footer("The estuary towards the Indian Ocean from the Bak Blau lake, Enggano Island",
+                     class = "fs-6; fw-lighter; blockquote-footer; border-0"),
+         class = "border-0")
+  ,
+  citation = card(class = "border-0",
+                  card_body(div(h5("How to cite EnoLEX")),
+                            div(p("Krauße, Daniel, Gede Primahadi Wijaya Rajeg, Cokorda Pramartha, Erik Zoebel, Charlotte Hemmings, I Wayan Arka, Mary Dalrymple (2024).", em("EnoLEX: A Diachronic Lexical Database for the Enggano language.")), style="font-size: 0.875em"),
+                            #div(h5("Please also cite the following paper:")),
+                            div(p("Rajeg, Gede Primahadi Wijaya, Daniel Krauße, and Cokorda Rai Adi Pramartha (2024).", a("EnoLEX: A Diachronic Lexical Database for the Enggano language", href='https://enggano.ling-phil.ox.ac.uk/static/papers/EnoLEX%20-%20A%20Diachronic%20Lexical%20Database%20for%20the%20Enggano%20language%20[Preprint].pdf', target="_blank"), ". In", em("Proceedings of AsiaLex 2024 (The Asian Association for Lexicography 2024 Hybrid Conference)."), "Toyo University, Tokyo: Japan."), style="font-size: 0.875em")
+                  )),
+  enolex_description = card(card_body(h3(strong("EnoLEX: A diachronic lexical database of the Enggano language")),
+                                      h4("Overview"),
+                                      p("EnoLEX collates lexical data from", actionLink("SourcesTabLink", "legacy materials and contemporary fieldwork data"), "about the Enggano language, ranging from simple/short and extensive word lists, anthropological and ethnographic writings, a dictionary, thesis, and contemporary Enggano data. The materials span over 150 years from the middle of the 19th century up to the present. With expert cognate-judgement, EnoLEX offers historical development of word forms expressing a certain concept/meaning."),
+                                      
+                                      h4("How to get started"),
+                                      p("Users can go to the", actionLink("CognatesTabLink", "Cognates"), "tab and then, from the left-hand side sidebar, select the concept to filter forms expressing that concept and how they develop across periods."),
+                                      
+                                      h4("Licensing"),
+                                      HTML('<p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://enggano.shinyapps.io/enolex/"><em>EnoLEX</em></a> edited by <span property="cc:attributionName">Daniel Krauße, Gede Primahadi W. Rajeg, Cokorda Pramartha, Erik Zoebel, Charlotte Hemmings, I Wayan Arka, and Mary Dalrymple</span> is licensed under <a href="https://creativecommons.org/licenses/by-nc/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">Creative Commons Attribution-NonCommercial 4.0 International<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1" alt=""></a></p>')
+                                      ),
+    
+                            textOutput("overview")
   ) #,
   #cognate_output <- card(
   #  full_screen = T,
@@ -74,21 +81,18 @@ cards <- list(
   #)
 )
 
-cognate_cards <- list(cognate_table = card(
-  full_screen = T,
-  card_header("Cognate groups"),
-  layout_sidebar(sidebar = english_gloss,
-                 div(dataTableOutput("cognatesOut"), style = "font-size:90%"))
-),
-background_image = card(
-  full_screen = T,
-  card_image(file = "estuary.JPG",
-             fill = TRUE,
-             class = "border-0",
-             width = "60%"),
-  card_footer("The estuary towards the Indian Ocean from the Bak Blau lake, Enggano Island",
-              class = "fs-6; fst-italic")
-)
+cognate_cards <- list(
+  cognate_table = card(
+    # card_header("Cognate groups"),
+    layout_sidebar(sidebar = sidebar(english_gloss, open = list(mobile = "always-above")),
+                   div(DT::DTOutput("cognatesOut"), style = "font-size:90%")),
+    height = 600),
+  concept_card = card(
+    htmlOutput("ConceptEnglishIndonesian"),
+    min_height = 110,
+    fill = TRUE,
+  )
+
 )
 
 ui <- page_navbar(
@@ -96,28 +100,59 @@ ui <- page_navbar(
   theme = bs_theme(
     version = bslib::version_default(),
     bootswatch = "cosmo",
-    base_font = font_google("Fira Sans")
+    bg = "#f9f8f5", 
+    fg = "#002147", 
+    #primary = "#3277ae",
+    primary = "#193658",
+    secondary="#003947",
+    "link-hover-color" = "#be0f34",
+    "link-color" = "#3277ae"
+    # source from Oxford colour parameters: https://www.ox.ac.uk/public-affairs/style-guide/digital-style-guide
   ),
-  fillable = "Cognates",
+  # collapsible = TRUE,
   underline = TRUE,
-  nav_panel(title = "Main", 
-            layout_columns(cards[["background_image"]], 
-                           cards[["citation"]], 
-                           cards[["enolex_description"]],
-                           col_widths = c(7, 5, 12),
-                           row_heights = c(5, 6))),
+  nav_panel(title = "Main",
+            
+            
+            layout_columns(
+              cards[["enolex_description"]],
+              # overview,
+              layout_columns(
+                cards[["citation"]],
+                cards[["background_image"]],
+                col_widths = c(12, 12)
+              )
+              
+            )
+            
+            
+            # layout_columns(cards[["enolex_description"]], 
+            #                cards[["citation"]],
+            #                cards[["background_image"]],
+            #                col_widths = c(5, 7, 12),
+            #                row_heights = c(5, 6))
+            # )
+  ),
   nav_panel(title = "Cognates",
-            layout_columns(cognate_cards[[1]])
+            
+            cognate_cards[["concept_card"]],
+            cognate_cards[["cognate_table"]]
+            # layout_columns(cognate_cards[["concept_card"]],
+            #                cognate_cards[["cognate_table"]],
+            #                col_widths = c(12, 12))
             # layout_sidebar(sidebar = english_gloss,
             #                dataTableOutput("cognatesOut"))
   ),
   nav_panel(title = "Sources",
             dataTableOutput("enolex_materials")),
+  nav_menu(title = "Links",
+           nav_item(link_enolex_github),
+           nav_item(link_enggano_web))
   # nav_panel(title = "Cognates", 
   #           p("To be filled with cognacy info.")),
   # nav_panel_hidden(value = "Cognates"),
-  fillable_mobile = TRUE,
-  title = "EnoLEX: A Diachronic Lexical Database of the Enggano Language"
+  # fillable_mobile = TRUE #,
+  # title =  "EnoLEX"
   # sidebar = sidebar(
   #   title = "Global Controls",
   #   english_gloss,
@@ -132,24 +167,28 @@ server <- function(input, output, session) {
   english_selected <- reactive(
     {
       if(input$English_Gloss != "(none)") {
-        tb <- select(filter(elx, English %in% input$English_Gloss), Cognate_ID, Year, Original_Form, Orthography, IPA, Indonesian, English, PMP_Etymon, PAN_Etymon)
-        # tb <- mutate(tb,
-        #              Cognate_ID = factor(Cognate_ID, levels = unique(tb$Cognate_ID)))
-        # cogtb_id <- unique(tb$Cognate_ID)
-        # cogtb_id_col <- brewer.pal(n = length(cogtb_id), name = "Paired")
+        tb <- select(filter(elx, English %in% input$English_Gloss), Cognate_ID, Year, Sources, Original_Form, Orthography, IPA, English_Original, PMP_Etymon, PAN_Etymon)
+        tb <- tb |> 
+          mutate(English_Original = replace_na(English_Original, ""))
+        
+        cog_id_colouring <- unique(tb$Cognate_ID)
+        
         DT::datatable(tb, escape = FALSE, options = list(paging = FALSE,
+                                                         scrollY = "500px",
+                                                         scrollX = TRUE,
                                                          autoWidth = TRUE,
                                                          columnDefs = list(list(className = "dt-center",
                                                                                 targets = 1))), 
                       filter = "top", 
-                      style = "bootstrap4") |> 
+                      style = "bootstrap4",
+                      class = list(stripe = FALSE)) |> 
           # mutate(Cognate_ID = factor(Cognate_ID)) |> 
           formatStyle("Cognate_ID",
-                      backgroundColor = styleEqual(unique(tb$Cognate_ID),
-                                                   colour_values(unique(tb$Cognate_ID), palette = "viridis", alpha = 65)))
+                      backgroundColor = styleEqual(cog_id_colouring,
+                                                   colour_values(factor(cog_id_colouring), palette = "viridis", alpha = 65)))
       } else {
         # DT::datatable(select(filter(elx, Cognate_ID %in% sample(unique(elx$Cognate_ID), size = 1)), Cognate_ID, Year, Original_Form, Orthography, IPA, Indonesian, English, PMP_Etymon, PAN_Etymon), escape = FALSE)
-        tb <- select(filter(elx, English == "dsadnfjsadnfkladfnasdlk"), Cognate_ID, Year, Original_Form, Orthography, IPA, Indonesian, English, PMP_Etymon, PAN_Etymon)
+        tb <- select(filter(elx, English == "dsadnfjsadnfkladfnasdlk"), Cognate_ID, Year, Sources, Original_Form, Orthography, IPA, PMP_Etymon, PAN_Etymon)
         DT::datatable(tb, escape = FALSE)
       }
     }
@@ -161,6 +200,69 @@ server <- function(input, output, session) {
   output$enolex_materials <- renderDT(materials_table())
   
   output$cognatesOut <- renderDT(english_selected())
+  
+
+    
+    output$ConceptEnglishIndonesian <- renderUI(
+    {
+      req(input$English_Gloss != "(none)")
+        
+        eng <- str_to_sentence(unique(pull(filter(elx, English %in% input$English_Gloss), English)))
+        # idn_interim <- unique(pull(filter(elx, English %in% input$English_Gloss), Indonesian))
+        
+        if (length(unique(pull(filter(elx, English %in% input$English_Gloss), Indonesian))) > 1) {
+          
+          idn <- str_c("‘<em>", str_c(unique(pull(filter(elx, English %in% input$English_Gloss), Indonesian)), collapse = ", "), "</em>’", sep = "")
+          
+          idn_orth <- elx |> 
+            filter(English %in% input$English_Gloss) |> 
+            select(Sources, Indonesian, Orthography) |> 
+            distinct()
+          idn_notes <- idn_orth |> 
+            group_by(Indonesian, Sources) |> 
+            mutate(forms = str_c("<em>", Orthography, "</em>", sep = "")) |> 
+            mutate(forms = str_c(forms, collapse = ", ")) |> 
+            group_by(Sources, Indonesian) |> 
+            mutate(forms = str_c(forms, " (", Sources, ")", sep = "")) |> 
+            ungroup() |> 
+            select(forms, Indonesian) |> 
+            distinct() |> 
+            group_by(Indonesian) |> 
+            mutate(forms = str_c(forms, collapse = "; ")) |> 
+            mutate(to_print = str_c("‘", Indonesian, "’ is the gloss for: ", forms, sep = "")) |> 
+            ungroup() |> 
+            pull(to_print) |> 
+            unique()
+          idn_notes <- str_c(str_c("<p>", idn_notes, "</p>", sep =""), collapse = "")
+          
+        } else {
+          
+          idn <- str_c("‘<em>", unique(pull(filter(elx, English %in% input$English_Gloss), Indonesian)), "</em>’", sep = "")
+          
+        }
+        
+        concepticon <- unique(pull(filter(elx, English %in% input$English_Gloss), Concepticon_Gloss))
+        if (!is.na(concepticon) & length(unique(pull(filter(elx, English %in% input$English_Gloss), Indonesian))) == 1) {
+          
+          HTML("<h3>", str_c(eng, " ", idn, "</h3><p>Corresponding concept set in Concepticon: ", concepticon, "</p>", sep = ""))  
+        
+          } else if (!is.na(concepticon) & length(unique(pull(filter(elx, English %in% input$English_Gloss), Indonesian))) > 1) {
+            
+            HTML("<h3>", str_c(eng, " ", idn, "</h3><p>Corresponding concept set in Concepticon: ", concepticon, "</p><p>", idn_notes, "</p>", sep = ""))
+            
+          } else if (is.na(concepticon) & length(unique(pull(filter(elx, English %in% input$English_Gloss), Indonesian))) == 1) {
+          
+            HTML("<h3>", str_c(eng, " ", idn, "</h3>", sep = ""))
+            
+          } else if (is.na(concepticon) & length(unique(pull(filter(elx, English %in% input$English_Gloss), Indonesian))) > 1) {
+          
+            HTML("<h3>", str_c(eng, " ", idn, "</h3>", "<p>", idn_notes, "</p>", sep = ""))
+        }
+        
+      
+    }
+  )
+  
   
   # the following code run the clicking on Cognates hyperlink the main panel/page
   observeEvent(input$CognatesTabLink, {
